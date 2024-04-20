c5026a5430f9aaeecfc526a9e08481b2f4cc5959
lavinialei2 <lavinialei@g.ucla.edu>
2024-04-20 10:50:34 -0700

    gemini photo challenge prompt

--- frontend/src/geminiAPI.jsx
+++ frontend/src/geminiAPI.jsx
@@ -17,14 +17,19 @@ async function run() {
     // For text-and-image input (multimodal), use the gemini-pro-vision model
     const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
   
-    const prompt = "What's different between these pictures?";
+    // Example use case:
+    // const prompt = "What's different between these pictures?";
   
-    const imageParts = [
-      fileToGenerativePart("image1.png", "image/png"),
-      fileToGenerativePart("image2.jpeg", "image/jpeg"),
-    ];
+    // const imageParts = [
+    //   fileToGenerativePart("image1.png", "image/png"),
+    //   fileToGenerativePart("image2.jpeg", "image/jpeg"),
+    // ];
   
-    const result = await model.generateContent([prompt, ...imageParts]);
+    // const result = await model.generateContent([prompt, ...imageParts]);
+
+    const prompt = "Give a list of 5 options for a simple photo challenge a group of strangers outside can do in 5 minutes. You will later be given these photos and asked to verify that they meet your requirements.";
+
+    const result = await model.generateContent(prompt);
     const response = await result.response;
     const text = response.text();
     console.log(text);
