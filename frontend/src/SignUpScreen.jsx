f495676bb84c5c0e8c33c53e30191a4affbebde8
Jasmine <jjasminew16@gmail.com>
2024-04-21 02:34:57 -0700

    fixed styling

--- frontend/src/SignUpScreen.jsx
+++ frontend/src/SignUpScreen.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import HomeScreen from './HomeScreen';
 import { Link } from 'react-router-dom';
-import GrassHopper from './assets/Group1.png';
+import GrassHopper from './assets/grasshopper.png';
 import './SignUpScreen.css';
 
 function SignUpScreen() {
@@ -11,11 +11,6 @@ function SignUpScreen() {
   const [password, onChangePassword] = useState('');
   const [hiddenPassword, onChangeHiddenPassword] = useState('');
   const [bool, setBool] = useState(false);
-
-//   const validateEmail = (text) => {
-//     const reg = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w\w+)+$/;
-//     return reg.test(text);
-//   };
 
   const handleChangePassword = (newText) => {
     const lastLetter = newText.slice(-1);
@@ -38,47 +33,10 @@ function SignUpScreen() {
     onChangeHiddenPassword(newTextWithDots);
   };
 
-  // const checkInputs = () => {
-  //   if (email === '') {
-  //     alert('Please enter an email to proceed');
-  //   } else if (!validateEmail(email)) {
-  //     alert('Please enter a valid email to proceed');
-  //   } else if (password === '') {
-  //     alert('Please enter a password to proceed');
-  //   } else if (password.length < 6) {
-  //     alert('Password must be longer than five characters');
-  //   } else {
-  //     return true;
-  //   }
-  //   return false;
-  // };
-
   const handleSignUp = async () => {
-    // if (!checkInputs()) {
-    //   return;
-    // }
     setFirstName('');
     setEmail('');
     onChangePassword('');
-    //window.navigate('/home');
-    // try {
-    //   const userData = {
-    //     email,
-    //     password,
-    //   };
-    //   const res = await axios.post(`${process.env.REACT_APP_SERVER_URL}/user/post`, userData); // Adjust server URL
-    //   if (res.data.error) {
-    //     console.error(res.data.error);
-    //   } else {
-    //     const userId = res.data._id;
-    //     // Adjust storage mechanism as needed for web
-    //     localStorage.setItem('userId', userId);
-    //     navigation.navigate('Home');
-    //   }
-    // } catch (err) {
-    //   console.log(err.message);
-    // }
-    //navigation.navigate('Home');
   };
 
 
@@ -86,7 +44,6 @@ function SignUpScreen() {
     <div className="signUp">
       <div className = 'wholePage'>
         <img src={GrassHopper} className='GrassHopper' />
-        {/* <h1 className = 'welcome' >welcome</h1> */}
         <div className="inputContainer">
           <input
             type="text"
@@ -114,9 +71,11 @@ function SignUpScreen() {
               placeholder="password"
             />
         </div>
+        <div className="alignSignUp">
           <Link to="/home">
             <button className="SignUp" onClick={handleSignUp}>sign up</button>
           </Link>
+        </div>
       </div>
     </div>
     );
