3c5a73b70580ee54b048cdce28ac371bc1eb1ba7
lavinialei2 <lavinialei@g.ucla.edu>
2024-04-20 21:42:02 -0700

    location button RAHHHHHH

--- frontend/src/DestinationScreen.jsx
+++ frontend/src/DestinationScreen.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import mapboxgl, { Control } from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import './DestinationScreen.css'
-import './Components/TimerWithProgressBar'; // Adjust the path if necessary
+import './Components/TimerWithProgressBar';
 import TimerWithProgressBar from './Components/TimerWithProgressBar';
 import earthIcon from './assets/earth-icon.png'
 
@@ -11,24 +11,41 @@ export default function DestinationScreen() {
 
     const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lng, setLng] = useState(-118.444);
-  const [lat, setLat] = useState(34.072);
+  const [lng, setLng] = useState(-118.440);
+  const [lat, setLat] = useState(34.075);
   const [zoom, setZoom] = useState(16);
 
   useEffect(() => {
-    if (map.current) return; // initialize map only once
+    if (map.current) return;
+
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: 'mapbox://styles/mapbox/streets-v12',
       center: [lng, lat],
-      zoom: zoom
+      zoom: zoom,
+      attributionControl: false
     });
 
+    map.current.addControl(
+      new mapboxgl.GeolocateControl({
+          positionOptions: {
+              enableHighAccuracy: true
+          },
+          trackUserLocation: true,
+          showUserHeading: true
+      }), 'top-right'
+    );
+
     map.current.on('move', () => {
-      setLng(map.current.getCenter().lng.toFixed(4));
-      setLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
+      setLng(map.current.getCenter().lng.toFixed(3));
+      setLat(map.current.getCenter().lat.toFixed(3));
+      setZoom(map.current.getZoom().toFixed(1));
     });
+
+    const marker = new mapboxgl.Marker({
+      color: "#ffa500"
+  }).setLngLat([-118.4382, 34.0778])
+      .addTo(map.current);
 
   });
 
@@ -43,7 +60,7 @@ export default function DestinationScreen() {
        
       <div ref={mapContainer} className="map-container" />
       
-       <div className="white-rectangle"> Longitude: {lng} | Latitude: {lat} | Zoom: {zoom} </div>
+       <div className="loc-data"> Longitude: {lng} | Latitude: {lat} | Zoom: {zoom} </div>
        <TimerWithProgressBar initTime={30} />
        <button className="hereButton">im here</button>
     </div>
