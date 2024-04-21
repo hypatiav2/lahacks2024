786ac8dc79c21c8511a1f8c17cb212f0b7ea66ba
hypatiav2 <73620120+hypatiav2@users.noreply.github.com>
2024-04-20 23:53:37 -0700

    resolve merge into main

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from "react-dom";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './index.css';
 import App from './App';
+import DestinationScreen from './DestinationScreen';
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+render(
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>,
+	document.getElementById("root"),
+
 );
