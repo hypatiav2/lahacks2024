665aad83ca3b97ae978f6377c334858256b6d66f
hypatiav2 <73620120+hypatiav2@users.noreply.github.com>
2024-04-20 23:48:49 -0700

    merge App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,27 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
-import DestinationScreen from './DestinationScreen'
+//import DestinationScreen from './DestinationScreen'
+import SignUpScreen from './SignUpScreen'
 import HomeScreen from './HomeScreen'
-import Upload from './Components/PhotoUpload'
+
+import { Route, Routes } from "react-router-dom";
+
 
 function App() {
+  // const [count, setCount] = useState(0)
 
   return (
-    
-    <DestinationScreen />
+    <>
+    <Routes>
+				<Route path="/" element={<SignUpScreen />} />
+				<Route path="home" element={<HomeScreen />} />
+    </Routes>
 
+    </>
   )
 }
+
 export default App
+
