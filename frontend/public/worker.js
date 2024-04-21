e43336fe3e21065cd2f62dea54c6f08808598dcf
hypatiav2 <73620120+hypatiav2@users.noreply.github.com>
2024-04-20 23:58:00 -0700

    resolve merge into main

--- frontend/public/worker.js
+++ frontend/public/worker.js
@@ -1,4 +1,4 @@
-
+/*
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('http://localhost:5173/serviceworker.js')
       .then(function(registration) {
@@ -7,3 +7,17 @@ if ('serviceWorker' in navigator) {
         console.log('Service worker registration failed, error:', err);
       });
     }
+*/
+
+console.log("Service worker loaded...");
+
+self.addEventListener('push', function(e) {
+    const data = e.data.json();
+    self.registration.showNotification(
+        data.title,
+        {
+            body: data.body,
+        }
+    );
+})
+
